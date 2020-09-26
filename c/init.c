143bf611fc70e51f3b14f4b34c0812fb3890deee
Miguel Moya <miguelmoya97@hotmail.com>
2020-09-26 14:22:39 -0700

    init implementation of kmalloc

--- c/init.c
+++ c/init.c
@@ -4,10 +4,10 @@
 #include <xeroskernel.h>
 #include <xeroslib.h>
 
-extern	int	entry( void );  /* start of kernel image, use &start    */
-extern	int	end( void );    /* end of kernel image, use &end        */
-extern  long	freemem; 	/* start of free memory (set in i386.c) */
-extern char	*maxaddr;	/* max memory address (set in i386.c)	*/
+extern int entry(void); /* start of kernel image, use &start    */
+extern int end(void);   /* end of kernel image, use &end        */
+extern long freemem;    /* start of free memory (set in i386.c) */
+extern char *maxaddr;   /* max memory address (set in i386.c)	*/
 
 /************************************************************************/
 /***				NOTE:				      ***/
@@ -23,49 +23,55 @@ extern char	*maxaddr;	/* max memory address (set in i386.c)	*/
  *  The init process, this is where it all begins...
  *------------------------------------------------------------------------
  */
-void initproc( void )				/* The beginning */
+void initproc(void) /* The beginning */
 {
-  
+
   char str[1024];
   int a = sizeof(str);
   int b = -17;
-  int i; 
+  int i;
 
-  kprintf( "\n\nCPSC 415, 2020W1\n32 Bit Xeros -20.9.9 - Closer to beta \nLocated at: %x to %x\n", 
-	   &entry, &end); 
-  
+  kprintf("\n\nCPSC 415, 2020W1\n32 Bit Xeros -20.9.9 - Closer to beta \nLocated at: %x to %x\n",
+          &entry, &end);
+
   kprintf("Some sample output to illustrate different types of printing\n\n");
 
   /* A busy wait to pause things on the screen, Change the value used 
      in the termination condition to control the pause
    */
 
-  for (i = 0; i < 3000000; i++);
+  for (i = 0; i < 3000000; i++)
+    ;
 
   /* Build a string to print) */
-  sprintf(str, 
-      "This is the number -17 when printed signed %d unsigned %u hex %x and a string %s.\n      Sample printing of 1024 in signed %d, unsigned %u and hex %x.",
-	  b, b, b, "Hello", a, a, a);
+  sprintf(str,
+          "This is the number -17 when printed signed %d unsigned %u hex %x and a string %s.\n      Sample printing of 1024 in signed %d, unsigned %u and hex %x.",
+          b, b, b, "Hello", a, a, a);
 
   /* Print the string */
 
-  kprintf("\n\nThe %dstring is: \"%s\"\n\nThe formula is %d + %d = %d.\n\n\n", 
-	  a, str, a, b, a + b);
+  kprintf("\n\nThe %dstring is: \"%s\"\n\nThe formula is %d + %d = %d.\n\n\n",
+          a, str, a, b, a + b);
 
-  for (i = 0; i < 4000000; i++);
+  for (i = 0; i < 4000000; i++)
+    ;
   /* or just on its own */
   kprintf(str);
 
   /* Add your code below this line and before next comment */
 
+  kmeminit();
+  // init data struct for managerlist     (1)
+  // init data struct for process queue   (2)
+  // init data struct for interrupt table (3)
 
+  for (i = 0; i < 2000000; i++)
+    ;
 
-  for (i = 0; i < 2000000; i++);
   /* Add all of your code before this comment and after the previous comment */
   /* This code should never be reached after you are done */
   kprintf("\n\nWhen your  kernel is working properly ");
   kprintf("this line should never be printed!\n");
-  for(;;) ; /* loop forever */
+  for (;;)
+    ; /* loop forever */
 }
-
-
--- c/mem.c
+++ c/mem.c
@@ -1,6 +1,67 @@
 /* mem.c : memory manager
  */
-
+#include <i386.h>
 #include <xeroskernel.h>
 
+extern long freemem; /* start of free memory (set in i386.c) */
+
+struct memHeader
+{
+    unsigned long size;
+    struct memHeader *prev;
+    struct memHeader *next;
+    char *sanityCheck;
+    unsigned char datastart[0];
+};
+
+// dont know if this head is accessible outside of mem.c ??
+// or if we need it accessible outisde???
+struct memHeader *head;
+
 /* Your code goes here */
+
+// missing sanityCheck init. and do we even need the unsigned char dataStart[0] ???
+
+extern void kmeminit(void)
+{
+    /* init head */
+
+    // set mem address of head to point at freemem (defined in i386.c)
+    head = (struct memHeader *)freemem;
+    head->size = HOLESTART - freemem - sizeof(struct memHeader);
+    // set next pointer to start of HOLEEND mem address
+    head->next = (struct memHeader *)HOLEEND;
+    head->prev = NULL;
+
+    /* init head->next */
+    // 4mb - holeend
+    struct memHeader *next_node = head->next;
+    next_node->size = (2 ^ 22) - HOLEEND - sizeof(struct memHeader);
+    next_node->next = NULL;
+    next_node->prev = head;
+}
+
+extern void *kmalloc(size_t size)
+{
+    long amnt;
+    struct memHeader *newNode;
+
+    amnt = (size) / 16 + ((size % 16) ? 1 : 0);
+    amnt = amnt * 16 + sizeof(struct memHeader);
+    struct memHeader *node = head;
+    while (node != NULL && node->size <= (amnt + sizeof(struct memHeader)))
+    {
+        // do shit
+        node = node->next;
+    }
+    newNode = (struct memHeader *)node->datastart;
+    newNode->size = (node->size - amnt) - sizeof(struct memHeader);
+    // add pointer manip
+    return NULL;
+}
+
+extern int kfree(void *ptr)
+{
+    //stubs
+    return 0;
+}
