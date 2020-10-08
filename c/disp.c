98d5885c891c3cb6c71345af6c61201bedb642dd
Miguel Moya <miguelmoya97@hotmail.com>
2020-10-08 16:00:33 -0700

    queue structure implemented

--- c/disp.c
+++ c/disp.c
@@ -3,4 +3,166 @@
 
 #include <xeroskernel.h>
 
-/* Your code goes here */
+void initqueue(void);
+void init_PCBs(void);
+//testing methods
+void test_queue(void);
+void print_readyQ(void);
+
+void ready_enqueue(struct pcb *proc);
+struct pcb *ready_dequeue(void);
+struct pcb *next(void);
+
+struct pcb list_of_pcbs[MAX_PCB_SIZE];
+struct pcb *ready_queue;
+
+void test_queue()
+{
+    // we'd have to figure out how to allocate these pcbs in our real implementation.
+    // probably just have a while loop and check for list_of_pcbs[i]->state == STOPPED
+    struct pcb test1 = list_of_pcbs[0];
+    test1.state = READY;
+    struct pcb test2 = list_of_pcbs[1];
+    test2.state = READY;
+    struct pcb test3 = list_of_pcbs[2];
+    test3.state = READY;
+
+    ready_enqueue(&test1);
+    ready_enqueue(&test2);
+    print_readyQ();
+    ready_dequeue();
+    print_readyQ();
+
+    ready_enqueue(&test3);
+    print_readyQ();
+}
+
+extern void initdispatch()
+{
+    init_PCBs();
+    initqueue();
+    test_queue();
+}
+
+extern void dispatch()
+{
+    struct pcb *process = next();
+    while (1)
+    {
+        // int request = contextswitch(process);
+        int request = -1;
+        switch (request)
+        {
+        case CREATE:
+            //create ()
+            break;
+        case YIELD:
+            ready(process);
+            process = next();
+            break;
+        case STOP:
+            //cleanup(process)
+            process = next();
+            break;
+        }
+    }
+}
+
+void initqueue()
+{
+    // ready queue will be set to null. Logic will be, if a PCB enters the ready queue, we just set the value of ready queue to the mem location of that process.
+    // and if more proceses get added to the queue, we just set the ready_queue->next to be that value and etc.
+    // ready_queue->next = 0;
+    ready_queue = 0;
+}
+
+void init_PCBs()
+{
+    // init list_of_pcbs with dead process's
+    for (int i = 0; i < MAX_PCB_SIZE; i++)
+    {
+        list_of_pcbs[i].pid = i;
+        list_of_pcbs[i].state = STOPPED;
+        list_of_pcbs[i].next = NULL;
+    }
+}
+
+struct pcb *next()
+{
+    return ready_dequeue();
+}
+extern int ready(void *proc)
+{
+    // why is it returning an int?
+    ready_enqueue((struct pcb *)proc);
+    return 1;
+}
+
+extern int cleanup(void *proc)
+{
+    // what to do with thiss
+    return 0;
+}
+
+/* 
+=====================================
+    QUEUE IMPLEMENTATION BELOW
+===================================== 
+*/
+
+void ready_enqueue(struct pcb *proc)
+{
+    // if ready queue is NOT empty
+    if (ready_queue)
+    {
+        // go to the last element;
+        while (ready_queue->next)
+        {
+            ready_queue = ready_queue->next;
+        }
+        ready_queue->next = proc;
+        proc->next = 0;
+    }
+    else
+    {
+        // if the ready_queue is empty, insert it and set the next to be NULL
+        ready_queue = proc;
+        ready_queue->next = NULL;
+    }
+}
+
+struct pcb *ready_dequeue()
+{
+    if (ready_queue)
+    {
+        // get reference to the first element (which is queue),
+        // then set queue to be queue->next
+        struct pcb *proc_to_ret = ready_queue;
+        ready_queue = ready_queue->next;
+        return proc_to_ret;
+    }
+    else
+    {
+        // idk if we need to do some work here.
+        kprintf("Queue is empty\n");
+        return 0;
+    }
+}
+
+void print_readyQ()
+{
+    struct pcb *iter = ready_queue;
+    int i = 0;
+    kprintf("====Queue====\n");
+
+    while (iter)
+    {
+        kprintf("Addr: %ld, state: %d, next: %ld\n",
+                iter,
+                iter->state,
+                iter->next);
+        iter = iter->next;
+        i++;
+    }
+    kprintf("====Queue size: %d====\n\n", i);
+}
--- h/xeroskernel.h
+++ h/xeroskernel.h
@@ -5,47 +5,91 @@
 
 /* Symbolic constants used throughout gutted Xinu */
 
-typedef	char    Bool;        /* Boolean type                  */
+typedef char Bool;           /* Boolean type                  */
 typedef unsigned int size_t; /* Something that can hold the value of
                               * theoretical maximum number of bytes 
                               * addressable in this architecture.
                               */
-#define	FALSE   0       /* Boolean constants             */
-#define	TRUE    1
-#define	EMPTY   (-1)    /* an illegal gpq                */
-#define	NULL    0       /* Null pointer for linked lists */
-#define	NULLCH '\0'     /* The null character            */
-
+#define FALSE 0              /* Boolean constants             */
+#define TRUE 1
+#define EMPTY (-1)  /* an illegal gpq                */
+#define NULL 0      /* Null pointer for linked lists */
+#define NULLCH '\0' /* The null character            */
 
 /* Universal return constants */
 
-#define	OK            1         /* system call ok               */
-#define	SYSERR       -1         /* system call failed           */
-#define	EOF          -2         /* End-of-file (usu. from read)	*/
-#define	TIMEOUT      -3         /* time out  (usu. recvtim)     */
-#define	INTRMSG      -4         /* keyboard "intr" key pressed	*/
-                                /*  (usu. defined as ^B)        */
-#define	BLOCKERR     -5         /* non-blocking op would block  */
-#define LASTCONST    -5
+#define OK 1        /* system call ok               */
+#define SYSERR -1   /* system call failed           */
+#define EOF -2      /* End-of-file (usu. from read)	*/
+#define TIMEOUT -3  /* time out  (usu. recvtim)     */
+#define INTRMSG -4  /* keyboard "intr" key pressed	*/
+                    /*  (usu. defined as ^B)        */
+#define BLOCKERR -5 /* non-blocking op would block  */
+#define LASTCONST -5
+
+/*
+    global vars definitions
+*/
+#define MAX_PCB_SIZE 32
+#define MAX_PROC_SIZE 16
+// Process State values
+#define READY 1
+#define RUNNING 2
+#define BLOCKED 3
+#define STOPPED 4
+// System call values
+#define CREATE 1
+#define YIELD 2
+#define STOP 3
 
 /* Functions defined by startup code */
 
-
-void           bzero(void *base, int cnt);
-void           bcopy(const void *src, void *dest, unsigned int n);
-void           disable(void);
+void bzero(void *base, int cnt);
+void bcopy(const void *src, void *dest, unsigned int n);
+void disable(void);
 unsigned short getCS(void);
-unsigned char  inb(unsigned int);
-void           init8259(void);
-int            kprintf(char * fmt, ...);
-void           lidt(void);
-void           outb(unsigned int, unsigned char);
-void           set_evec(unsigned int xnum, unsigned long handler);
+unsigned char inb(unsigned int);
+void init8259(void);
+int kprintf(char *fmt, ...);
+void lidt(void);
+void outb(unsigned int, unsigned char);
+void set_evec(unsigned int xnum, unsigned long handler);
+
+struct CPU
+{
+    int esp;
+    int eip;
+    int eflags;
+    int edi;
+    int esi;
+    int edx;
+    int ecx;
+    int ebx;
+    int eax;
+};
+
+struct pcb
+{
+    int pid;
+    int state;
+    void *proc_stack;
+    struct CPU *cpu_state;
+    struct pcb *next;
+};
 
 extern void kmeminit(void);
 extern void *kmalloc(size_t size);
 extern int kfree(void *ptr);
 
+void print_list(void);
+
+extern void initdispatch(void);
+extern void dispatch(void);
+extern int ready(void *);
+extern int cleanup(void *);
+
+// extern struct pcb *next(void);
+// extern struct void ready(struct pcb *);
 
 /* Anything you add must be between the #define and this comment */
 #endif
